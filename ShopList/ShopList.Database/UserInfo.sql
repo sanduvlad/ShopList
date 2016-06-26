a146192554477dd9c95692354be0b39365c23245
Sandu Vlad <sanduvlad79@gmail.com>
2016-06-27 00:04:00 +0300

    Other changes to UserInfo table

---
+++ ShopList/ShopList.Database/UserInfo.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[UserInfo]
+(
+	[UserId] INT NOT NULL  IDENTITY, 
+    [FirstName] NVARCHAR(MAX) NOT NULL, 
+    [LastName] NVARCHAR(MAX) NOT NULL, 
+    [Age] INT NULL, 
+    [Phone] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [FK_UsersInfo_ToUsers] FOREIGN KEY ([UserId]) REFERENCES [Users]([Id])
+)
--- ShopList/ShopList.Database/UsersInfo.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[UsersInfo]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Nume] NVARCHAR(MAX) NOT NULL, 
-    [Prenume] NVARCHAR(MAX) NOT NULL, 
-    [Varsta] INT NULL, 
-    [Telefon] NVARCHAR(MAX) NULL, 
-    CONSTRAINT [FK_UsersInfo_ToUsers] FOREIGN KEY ([Id]) REFERENCES [Users]([Id])
-)
